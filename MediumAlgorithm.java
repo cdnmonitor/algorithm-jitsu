d32ce75b261848b732d5b62c3aedeaf31bf38667
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-05-04 20:11:10 -0700

    adding medium alg

--- MediumAlgorithm.java
+++ MediumAlgorithm.java
@@ -3,10 +3,6 @@ public class MediumAlgorithm implements DifficultyAlgorithm {
     @Override
     public String selectCard(String[] cards) {
         int maxIndex=0;
-        // split each string into an array of strings on the comma
-        // print out each card
-        
-    
         for (int i=0; i<cards.length;i++){
             String[] card = cards[i].split(",");
             if (Integer.parseInt(card[1]) > Integer.parseInt(cards[maxIndex].split(",")[1])){
@@ -14,19 +10,5 @@ public class MediumAlgorithm implements DifficultyAlgorithm {
             }
         }
         return cards[maxIndex];
-    
-
-        
     }
-
-    // public Card selectCard(ArrayList<Card> cards) {
-    //     int maxIndex=0;
-    //     for (int i=0; i<cards.size();i++){
-    //         if (cards.get(i).getPowerNumber() > cards.get(maxIndex).getPowerNumber()){
-    //             maxIndex = i;
-    //         }
-    //     }
-    //     return cards.get(maxIndex);
-    // }
-
 }
