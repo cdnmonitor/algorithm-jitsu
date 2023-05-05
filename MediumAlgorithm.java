4b7b943fe85c5e1c40916a8ae82289a425ad4198
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-05-04 19:41:28 -0700

    medium algo

--- MediumAlgorithm.java
+++ MediumAlgorithm.java
@@ -1,15 +1,32 @@
-
 import java.util.ArrayList;
-
-public class MediumAlgorithm  {
-    public Card selectCard(ArrayList<Card> cards) {
+public class MediumAlgorithm implements DifficultyAlgorithm {
+    @Override
+    public String selectCard(String[] cards) {
         int maxIndex=0;
-        for (int i=0; i<cards.size();i++){
-            if (cards.get(i).getPowerNumber() > cards.get(maxIndex).getPowerNumber()){
+        // split each string into an array of strings on the comma
+        // print out each card
+        
+    
+        for (int i=0; i<cards.length;i++){
+            String[] card = cards[i].split(",");
+            if (Integer.parseInt(card[1]) > Integer.parseInt(cards[maxIndex].split(",")[1])){
                 maxIndex = i;
             }
         }
-        return cards.get(maxIndex);
+        return cards[maxIndex];
+    
+
+        
     }
+
+    // public Card selectCard(ArrayList<Card> cards) {
+    //     int maxIndex=0;
+    //     for (int i=0; i<cards.size();i++){
+    //         if (cards.get(i).getPowerNumber() > cards.get(maxIndex).getPowerNumber()){
+    //             maxIndex = i;
+    //         }
+    //     }
+    //     return cards.get(maxIndex);
+    // }
 
 }
