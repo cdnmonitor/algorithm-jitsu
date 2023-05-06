011c7de8e90100e8368fa4864b6bd4f7fe8b1412
Jack Donnelly <jdonnelly@chapman.edu>
2023-05-05 17:28:34 -0700

    Fixed AI not reading cards between rounds and added difficulty selection to simpleAI class

--- SimpleAI.java
+++ SimpleAI.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.util.Scanner;
 
 public class SimpleAI {
     private String serverAddress;
@@ -37,6 +39,8 @@ public class SimpleAI {
                     displayCombat(in);
                 } else if (line.startsWith("RESULT")) {
                     System.out.println(line);
+                } else if (line.startsWith("CARDS")){
+                    readCards(in);
                 } else if (line.startsWith("SCORE")) {
                     System.out.println(line);
                     break;
@@ -54,7 +58,7 @@ public class SimpleAI {
         System.out.println("Your cards are:");
         for (int i = 0; i < numCards; i++) {
             String cardString = in.readLine();
-            System.out.println("AI:" + cardString);
+            System.out.println("AI " + i + " :" + cardString);
             cards[i] = cardString;
         }
     }
@@ -73,10 +77,50 @@ public class SimpleAI {
     }
 
     public static void main(String[] args) throws Exception {
-        // Replace this part with user input to choose the difficulty level
-        DifficultyAlgorithm difficultyAlgorithm = new MediumAlgorithm();
+        DifficultyAlgorithm difficultyAlgorithm;
+        Scanner in = new Scanner(System.in);
+
+        boolean goodInput = false;
+        int choiceNum = 5;
+
+        System.out.println("Please Select a Difficulty:");
+
+        while (!goodInput) {
+            System.out.println("0: Easy \n1: Medium \n2: Hard");
+            String choice = in.nextLine();
+            try {
+                choiceNum = Integer.parseInt(choice);
+                if (choiceNum == 0 || choiceNum == 1 || choiceNum == 2) {
+                    goodInput = true;
+                } else {
+                    System.out.println("Please Enter a Valid Choice From 0 to 2");
+                }
+            } catch (Exception e) {
+                System.out.println("Please Enter a Number from 0 to 2");
+            }
+        }
+
+        //Set Difficulty
+        switch (choiceNum) {
+            case 0:
+                difficultyAlgorithm = new EasyAlgorithm();
+                System.out.println("Easy Algorithm Selected");
+                break;
+            case 1:
+                difficultyAlgorithm = new MediumAlgorithm();
+                System.out.println("Medium Algorithm Selected");
+                break;
+            case 2:
+                difficultyAlgorithm = new HardAlgorithm();
+                System.out.println("Hard Algorithm Selected");
+                break;
+            // Will never happen
+            default:
+                difficultyAlgorithm = new EasyAlgorithm();
+        }
 
         SimpleAI simpleAI = new SimpleAI("localhost", difficultyAlgorithm);
         simpleAI.playAI();
+        in.close();
     }
 }
