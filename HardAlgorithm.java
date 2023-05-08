c593d08096d9de25c960255cbf454566794477b6
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-05-07 17:09:31 -0700

    HardAlgorithm implimentation and shell script for playing client vs Ai

--- HardAlgorithm.java
+++ HardAlgorithm.java
@@ -1,52 +1,46 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class HardAlgorithm implements DifficultyAlgorithm {
+    private boolean useAlternateStrategy = false;
+    private int lossCount = 0;
 
     @Override
     public String selectCard(String[] cards) {
-        // Analyze the cards and count the occurrence of each element
-        HashMap<String, Integer> elementCount = new HashMap<>();
-        for (String card : cards) {
-            String[] cardData = card.split(",");
-            String element = cardData[0];
-            elementCount.put(element, elementCount.getOrDefault(element, 0) + 1);
-        }
-
-        // Find the most common element
-        Map.Entry<String, Integer> mostCommonElement = null;
-        for (Map.Entry<String, Integer> entry : elementCount.entrySet()) {
-            if (mostCommonElement == null || entry.getValue() > mostCommonElement.getValue()) {
-                mostCommonElement = entry;
-            }
-        }
-
-        // Select the card with the highest power number of the most common element
+        int highestScore = Integer.MIN_VALUE;
         String selectedCard = null;
-        int highestPower = Integer.MIN_VALUE;
+
         for (String card : cards) {
             String[] cardData = card.split(",");
             String element = cardData[0];
             int powerNumber = Integer.parseInt(cardData[1]);
-            if (element.equals(mostCommonElement.getKey()) && powerNumber > highestPower) {
-                selectedCard = card;
-                highestPower = powerNumber;
-            }
-        }
+            String color = cardData[2];
 
-        // If the most common element is not found in the hand, fall back to the MediumAlgorithm logic
-        if (selectedCard == null) {
-            int maxIndex = 0;
-            for (int i = 0; i < cards.length; i++) {
-                String[] card = cards[i].split(",");
-                if (Integer.parseInt(card[1]) > Integer.parseInt(cards[maxIndex].split(",")[1])) {
-                    maxIndex = i;
-                }
+            int score = powerNumber;
+
+            if (!useAlternateStrategy) {
+                // Code for unique color and element
+                score += (element.hashCode() * color.hashCode());
+            } else {
+                // Code for different element and color
+                score += (element.hashCode() - color.hashCode());
             }
-            selectedCard = cards[maxIndex];
+
+            if (score > highestScore) {
+                highestScore = score;
+                selectedCard = card;
+            }
         }
 
         return selectedCard;
+    }
+
+    public void incrementLossCount() {
+        this.lossCount++;
+        if (lossCount >= 2) {
+            this.useAlternateStrategy = !useAlternateStrategy;
+            this.lossCount = 0;
+        }
+    }
+
+    public void resetLossCount() {
+        this.lossCount = 0;
     }
 }
--- runAIGameMAC.sh
+++ runAIGameMAC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script will run the server, client, and simple AI in separate terminal windows for testing purposes on a Mac.
-# To run this script, open a terminal window and type: sh playai.sh
+# To run this script, open a terminal window and type: ./runAIGameMAC.sh
 cwd=$(pwd)
 osa=$(osascript -e 'tell application "Terminal" to do script "cd '$cwd'; java Server"')
 osa=$(osascript -e 'tell application "Terminal" to do script "cd '$cwd'; java Client"')
