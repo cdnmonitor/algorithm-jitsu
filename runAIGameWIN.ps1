6f4afc3a2887b8ddc272c2d58b81e0f0af652a0d
Jack Donnelly <78446678+LeonTheMouse@users.noreply.github.com>
2023-05-09 10:58:26 -0700

    Update for windows compatability

--- runAIGameWIN.ps1
+++ runAIGameWIN.ps1
@@ -3,6 +3,6 @@
 # Then, run the command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser" to allow the script to run.
 # Finally, run the command ".\runAIGameWIN.ps1" to run the script.
 $cwd = Get-Location
-Start-Process -NoNewWindow -WorkingDirectory $cwd -FilePath "java" -ArgumentList "Server"
-Start-Process -NoNewWindow -WorkingDirectory $cwd -FilePath "java" -ArgumentList "Client"
-Start-Process -NoNewWindow -WorkingDirectory $cwd -FilePath "java" -ArgumentList "SimpleAI"
+Start-Process -WorkingDirectory $cwd -FilePath "java" -ArgumentList "Server"
+Start-Process -WorkingDirectory $cwd -FilePath "java" -ArgumentList "Client"
+Start-Process -WorkingDirectory $cwd -FilePath "java" -ArgumentList "SimpleAI"
